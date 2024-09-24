a7a652c4443aaace8864fb602d4362b51686f2ea
Mwarandus Lab <info@mwaranduslab.com>
2024-09-24 20:38:50 +0300

    Test remote code

--- read _remote_signal.ino
+++ read _remote_signal.ino
@@ -1,1 +1,21 @@
+#include <IRremote.h>
+
+const int irReceiverPin = 9;
+IRrecv irrecv(irReceiverPin);
+decode_results results;
+
+void setup() {
+  Serial.begin(9600);
+  irrecv.enableIRIn();  // Start the receiver
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    // Print the received value in hexadecimal
+    Serial.print("Received value: 0x");
+    Serial.println(results.value, HEX);
+    
+    irrecv.resume(); // Ready to receive the next signal
+  }
+}
 
